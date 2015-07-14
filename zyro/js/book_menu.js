ff25d620bcb834a3e6d0a3999d0b282aba69d473
Taipei Tour Guide Association <tourcheng@gmail.com>
2015-07-14 11:15:42 +0000

    initial commit

---
+++ zyro/js/book_menu.js
@@ -0,0 +1,7 @@
+function MenuOn(x){ 
+	obj=document.getElementById("submenu"+x).style.visibility="visible";
+}
+
+function MenuOff(x){ 
+	obj=document.getElementById("submenu"+x).style.visibility="hidden"; 
+}
